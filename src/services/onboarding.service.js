28d10884df2f3d7923e400692fb00d2b2dbce594
Naman Narain <namannarain@Namans-MacBook-Pro.local>
2026-01-31 18:50:17 +0530

    Update welcome message

--- src/services/onboarding.service.js
+++ src/services/onboarding.service.js
@@ -4,18 +4,18 @@ const { userExists, onboardUser } = require('../../db.js');
 const { safeRewrite, sendWhatsAppMessage } = require('./whatsapp.service');
 
 const WELCOME_MESSAGE =
-  "Hi! 👋 Welcome to the *Birthday Bot* 🎂\n\n" +
-  "This is a new bot and still being tested. Responses may be slow and some things may not work as expected. 😊\n\n" +
+  "Hi! 👋 Welcome to the *Birthday and Anniversary Reminder* created by Naman 🎂💍\n\n" +
+  "This is a new bot and still being tested. Responses may be slow and some things may not work as expected. Please leave a message at +91-8769010233, for any feedback.😊\n\n" +
   "To save a birthday, just type (example):\n" +
-  " → *Papa, 29 Aug*\n" +
-  " → *Mom, 9 Feb*\n\n" +
-  "To search ordelete a birthday, just type:\n" +
-  " → *search Papa*\n" +
-  " → *delete Mom*\n\n" +
+  " → *Papa 29 Aug*\n" +
+  " → *Mom 9 Feb*\n\n" +
+  "To search or delete a birthday, just type (example):\n" +
+  " → *search papa*\n" +
+  " → *delete mom*\n\n" +
   "To see all birthdays, type:\n" +
-  " → *Complete list*\n\n" +
-  "That's it 👍\n" +
-  "Just send messages like normal WhatsApp. No buttons, no forms.";
+  " → *complete list*\n\n" +
+  "That's it 👍\n\n" +
+  " Never miss birthdays or anniversaries again. I've got you covered :)";
 
 // Check if user needs onboarding and send welcome if needed
 async function handleOnboarding(phone) {
